0e6be0bb71dfbc8560373044ab95d475aa1e0199
Antonio Souza <antoniosouza.dev@gmail.com>
2022-02-27 11:35:16 -0300

    Criada a classe de iniciar a forca

--- src/main/forca.cpp
+++ src/main/forca.cpp
@@ -6,5 +6,10 @@ using namespace std;
 class Forca {
     public: 
         string palavra; // A palavra que deverá ser descoberta;
-    
+
+        int inicia(string palavra) {
+            this -> palavra = palavra;
+            cout << "Iniciando o jogo com a palavra: " << this -> palavra << "!\n\n";
+            return 0;
+        }
 };
