158efbbab11052fbad42e5164b41b7541199fa8b
Antonio Souza <antoniosouza.dev@gmail.com>
2022-02-27 11:58:42 -0300

    registra as letras em um vetor

--- src/main/forca.cpp
+++ src/main/forca.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -8,8 +9,20 @@ class Forca {
         string palavra; // A palavra que deverá ser descoberta;
 
         int inicia(string palavra) {
+            
             this -> palavra = palavra;
             cout << "Iniciando o jogo com a palavra: " << this -> palavra << "!\n\n";
+            registraLetras();
             return 0;
+        }
+    private:
+        int i, tamanho;
+        vector<char> letras;
+        void registraLetras() {
+            tamanho = palavra.length();
+            for (i = 0; i < tamanho; i++) {
+                cout << "Tentando adicionar às letras: " << palavra[i] << "\n";
+                letras.push_back(palavra[i]);
+            }
         }
 };
