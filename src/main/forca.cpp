0004dbbf2ffb6e067af24e6f5a4300c3ac8fdb23
Antonio Souza <antoniosouza.dev@gmail.com>
2022-02-27 12:13:48 -0300

    verifica letra dada e limpa tela

--- src/main/forca.cpp
+++ src/main/forca.cpp
@@ -8,20 +8,40 @@ class Forca {
     public: 
         string palavra; // A palavra que deverá ser descoberta;
 
+        void resetaTela() {
+            int i;
+            cout << flush;
+            for (i = 0; i < palavra.length(); i++) {
+                cout << "*";
+            }
+        }
+
+        void adivinhaLetra(char letraEscolhida) {
+            int acertos = 0;
+            int i;
+            resetaTela();
+            for (i = 0; i < palavra.length(); i++) {
+                if (palavra[i] == letraEscolhida) {
+                    acertos ++;
+                    cout << letraEscolhida;
+                } else {
+                    cout << "*";
+                }
+            }
+        }
+
         int inicia(string palavra) {
-            
             this -> palavra = palavra;
-            cout << "Iniciando o jogo com a palavra: " << this -> palavra << "!\n\n";
             registraLetras();
+            cout << "JOGO INICIADO!!";
             return 0;
         }
     private:
-        int i, tamanho;
         vector<char> letras;
         void registraLetras() {
+            int i, tamanho;
             tamanho = palavra.length();
             for (i = 0; i < tamanho; i++) {
-                cout << "Tentando adicionar às letras: " << palavra[i] << "\n";
                 letras.push_back(palavra[i]);
             }
         }
