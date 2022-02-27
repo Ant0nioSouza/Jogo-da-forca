7fe1004e27d780cd099f31f9a6479e4ed0a028c9
Antonio Souza <antoniosouza.dev@gmail.com>
2022-02-27 14:15:50 -0300

    verifica letras descobertas

--- src/main/forca.cpp
+++ src/main/forca.cpp
@@ -7,6 +7,8 @@ using namespace std;
 class Forca {
     public: 
         string palavra; // A palavra que deverá ser descoberta;
+        bool finalizado;
+        int tentativas;
 
         void resetaTela() {
             int i;
@@ -18,26 +20,65 @@ class Forca {
 
         void adivinhaLetra(char letraEscolhida) {
             int acertos = 0;
-            int i;
-            resetaTela();
+            int i, j;
+            cout << "\033[2J\033[1;1H"; // limpa a tela;
             for (i = 0; i < palavra.length(); i++) {
-                if (palavra[i] == letraEscolhida) {
-                    acertos ++;
-                    cout << letraEscolhida;
+                if (letrasDescobertas.size() > 0) {
+                for (j = 0; j < letrasDescobertas.size(); j++) {
+                    if (palavra[i] == letrasDescobertas[j]) {
+                        cout << letrasDescobertas[j];
+                    } else if (palavra[i] == letraEscolhida) {
+                        acertos ++;
+                        cout << letraEscolhida;
+                    } else {
+                        cout << "*";
+                    }
+                }
                 } else {
-                    cout << "*";
+                    if (palavra[i] == letraEscolhida) {
+                        acertos ++;
+                        cout << letraEscolhida;
+                    } else {
+                        cout << "*";
+                    }
                 }
             }
+
+
+            if (acertos > 0) {
+                letrasDescobertas.push_back(letraEscolhida);
+            } else {
+                letrasTentadas.push_back(letraEscolhida);
+                tentativas --;
+            }
+
+            if (tentativas <= 0) {
+                cout << "\033[2J\033[1;1H";
+                cout << "JOGO FINALIZADO! SUA PONTUAÇÃO: " << letrasDescobertas.size() - letrasTentadas.size();
+                finalizado = true;
+            }
+            if (letrasDescobertas == letras) {
+                cout << "\033[2J\033[1;1H";
+                cout << "JOGO FINALIZADO! SUA PONTUAÇÃO: " << letrasDescobertas.size() - letrasTentadas.size();
+                finalizado = true;
+            }
+
         }
 
-        int inicia(string palavra) {
+        int inicia(string palavra, int tentativas) {
             this -> palavra = palavra;
+            this -> tentativas = tentativas;
+
             registraLetras();
+            finalizado = false;
             cout << "JOGO INICIADO!!";
             return 0;
         }
     private:
         vector<char> letras;
+        vector<char> letrasDescobertas;
+        vector<char> letrasTentadas;
+
         void registraLetras() {
             int i, tamanho;
             tamanho = palavra.length();
@@ -46,3 +87,19 @@ class Forca {
             }
         }
 };
+
+int main() {
+    Forca forca;
+    string palavra;
+    cout << "Digite a palavra do jogo: ";
+    cin >> palavra;
+    forca.inicia(palavra, 3);
+
+    while (!forca.finalizado) {
+        cout << "Digite uma letra para adivinhar: ";
+        char letra;
+        cin >> letra;
+        forca.adivinhaLetra(letra);
+    }
+    return 0;
+}
